3ab9b8e7d9b634ae2212e8bffdfccaea77013ecf
Ouerghi <samehouerghi6@gmail.com>
2021-10-13 23:19:00 -0400

    first commit

---
+++ src/main/java/ma/enset/demomss/DemoMssApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.demomss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMssApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ma/enset/demomss/entities/Product.java
@@ -0,0 +1,21 @@
+package ma.enset.demomss.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Product {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private double quantity;
+}
